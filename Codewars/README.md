d4a94fada0f5f59f78709ad8fce422b6ca69533d
danmartin98 <danmartin98@icloud.com>
2022-02-04 12:12:18 +0000

    created 3 components that we can test

---
+++ Codewars/README.md
@@ -0,0 +1,3 @@
+# Code wars
+
+## Your Task
---
+++ src/Components/AddImageButton/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AddImageButton() {
+  return (
+    <div>
+      <button>Add image</button>
+    </div>
+  );
+}
