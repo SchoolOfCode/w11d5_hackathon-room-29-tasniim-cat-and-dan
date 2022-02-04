5f46a744e0587af430771e3467c965421c0cc301
patrick <tasniim_bari@yahoo.com>
2022-02-04 13:40:45 +0000

    firstreactTest

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from "./soc-logo.svg";
 import "./App.css";
+import AddButton from "../src/Components/AddButton"
+import AddImageButton from "../src/Components/AddImageButton"
+import ChangeTitleButton from "../src/Components/ChangeTitleButton"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hackathon Friday</p>
-      </header>
+      <AddButton/>
+     <AddImageButton/>
+     <ChangeTitleButton/>
     </div>
   );
 }
--- src/Components/AddButton/index.js
+++ src/Components/AddButton/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function AddButton() {
   return (
     <div>
-      <button>+1</button>
+      <button data-testID={`checkforbutton`}>hello</button>
     </div>
   );
 }
